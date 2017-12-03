ba00fc14b986b523d43afb396710f2af59deb9c8
PogadaevEA <18346ggg>
2017-12-03 15:11:16 +0700

    копирование компонентов из примера

---
+++ tracker-core/src/main/java/jdev/tracker/services/FortuneService.java
@@ -0,0 +1,14 @@
+package jdev.tracker.services;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by jdev on 05.05.2017.
+ */
+@Service
+public class FortuneService {
+
+    public double tryFortune() {
+        return Math.random();
+    }
+}
